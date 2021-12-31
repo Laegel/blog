1238dd7ec7b8c0fabbbed4e095f1aaf0ebf39a9c
Laegel <valentin.chouaf@laposte.net>
2021-12-31 11:49:13 +0100

    fix: Remove base path

--- workaround.ts
+++ workaround.ts
@@ -2,7 +2,7 @@ const watcher = Deno.watchFs("./app/.aleph/production");
 for await (const event of watcher) {
   console.log(">>>> event", event);
   // { kind: "create", paths: [ "/foo.txt" ] }
-  if (event.paths[0].endsWith("app.tailwind.css.js")) {
-    Deno.writeFile("app/.aleph/production/app.tailwind.css.bundling.js", new Uint8Array());
+  if (event.paths[0].endsWith(".tailwind.css.bundling.js")) {
+    Deno.writeFile(event.paths[0], new Uint8Array());
   }
 }
