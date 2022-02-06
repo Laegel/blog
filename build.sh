917ecb63342fdc8ac7e5d422db6b8a0b662bbcc8
Laegel <valentin.chouaf@laposte.net>
2022-02-06 23:28:12 +0100

    fix(devops): Debug action

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd docs/
 rm -rf _aleph && rm -rf fonts && rm -rf idees && rm -rf images && rm -rf reflexions && rm -rf technos
-cd /workspace/app && aleph build && mv docs/* ../docs
+cd ../app && aleph build && mv docs/* ../docs
