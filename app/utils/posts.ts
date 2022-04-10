e1487bbc7fc950b7873a961969ec34ab2843a007
Laegel <valentin.chouaf@laposte.net>
2022-04-10 16:58:36 +0200

    fix: Sorting & excerpt

--- app/utils/posts.ts
+++ app/utils/posts.ts
@@ -2,20 +2,23 @@ import { safeLoadFront } from "yaml-front-matter";
 
 import type { PostMeta } from "~/types.ts";
 import { toFrenchFormat } from "./date.ts";
+import strip from "remove-markdown";
 
 export const toArray = async (iterator: AsyncIterable<Deno.DirEntry>, root: string): Promise<PostMeta[]> => {
   const items = [];
   for await (const val of iterator) {
     const { __content, ...meta } = safeLoadFront(await Deno.readTextFile(root + val.name));
+    
     items.push({
       ...meta,
       path: val.name.replace(".md", ""),
-      excerpt: __content.substring(0, 90),
+      excerpt: strip(__content).substring(0, 90),
       created: toFrenchFormat(meta.created),
       updated: meta.updated ? toFrenchFormat(meta.updated) : undefined,
+      sortKey: meta.created,
     } as PostMeta);
   }
-  items.sort(((item1, item2) => item1.created > item2.created ? -1 : 1));
+  items.sort(((item1, item2) => item2.sortKey - item1.sortKey));
   return items;
 };
 
